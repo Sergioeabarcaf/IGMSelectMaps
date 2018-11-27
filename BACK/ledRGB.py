d35264f1a0d56d63948e0a1c2f035b358178608f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-11-27 13:28:56 -0300

    BACK - identacion

--- BACK/ledRGB.py
+++ BACK/ledRGB.py
@@ -10,12 +10,13 @@ GPIO.setup(GREEN,GPIO.OUT)
 GPIO.output(GREEN,0)
 GPIO.setup(BLUE,GPIO.OUT)
 GPIO.output(BLUE,0)
+
 try:
   while (True):
-      request = raw_input(“RGB—>”) 
-    if (len(request) == 3): 
-        GPIO.output(RED,int(request[0]))
-        GPIO.output(GREEN,int(request[1]))
-        GPIO.output(BLUE,int(request[2]))
-    except KeyboardInterrupt:
+      request = raw_input(“RGB—>”)
+      if (len(request) == 3):
+          GPIO.output(RED,int(request[0]))
+          GPIO.output(GREEN,int(request[1]))
+          GPIO.output(BLUE,int(request[2]))
+except KeyboardInterrupt:
     GPIO.cleanup()
