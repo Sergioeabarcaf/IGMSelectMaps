0520ea161c585d27cfa1e768f244b8c763061a81
sergioeabarcaf <sergioaf1991@gmail.com>
2018-12-17 15:33:42 -0300

    BACK - boton para apagar sistema desde portal y cargar chromium full screen

--- BACK/main.py
+++ BACK/main.py
@@ -6,9 +6,12 @@ import serial
 import paho.mqtt.client
 import paho.mqtt.publish as publish
 import ledRGB
+import os
 
 ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate = 115200
+
+host = "192.168.1.197"
 
 def comSerial(map):
     if(ser.is_open):
@@ -17,21 +20,24 @@ def comSerial(map):
             x = ser.read()
             print x
             if x == 'T':
-                publish.single('load','true',hostname="192.168.1.198")
+                publish.single('load','true',hostname=host)
                 break
             if x == '0':
-                publish.single('error','La letra enviada no corresponde a los mapas.',hostname="192.168.1.198")
+                publish.single('error','La letra enviada no corresponde a los mapas.',hostname=host)
                 break
     else:
         ledRGB.errorSerial()
-        publish.single('error','Comunicacion serial esta cerrada',hostname="192.168.1.198")
+        publish.single('error','Comunicacion serial esta cerrada',hostname=host)
 
 def on_connect(client, userdata, flags, rc):
     print('connected (%s)' % client._client_id)
     client.subscribe(topic='send', qos=2)
  
 def on_message(client, userdata, message):
-    return comSerial(message.payload)
+    if(message.payload == "off"):
+        os.system("sudo reboot")
+    else:
+        comSerial(message.payload)
 
  
 def main():
@@ -39,7 +45,9 @@ def main():
     client = paho.mqtt.client.Client(client_id='pythonIGM', clean_session=False)
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect(host='192.168.1.198', port=1883)
+    client.connect(host=host, port=1883)
+    time.sleep(5)
+    os.system("chromium-browser --start-fullscreen")
     client.loop_forever()
 
 if __name__ == '__main__':
